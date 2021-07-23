c429f5b0b7520e7e65a278191a9e83d0cd54985d
cex <cex@cexs-MacBook-Pro.local>
2021-07-23 16:57:23 +0300

    jaimusic update

---
+++ iggy/JAIMUSIC/script.js
@@ -0,0 +1,8 @@
+var a=document.getElementById("igemperors");
+var imga=document.getElementById("emperorsimg");
+
+window.addEventListener("load", function() {
+  imga.addEventListener("mouseover" , function() {
+    a.play();
+  });
+});
