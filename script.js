35fac7949da87904227bb2f6f7260c4bb18fb495
Codie Whitaker <CWhitaker2@student.fullsail.edu>
2021-07-23 22:07:18 -0400

    Layout fine Tuning and More Directory Organization

--- script.js
+++ script.js
@@ -4,14 +4,15 @@ $( document ).ready(function() {
   $(".thumbnail-color").hide();
 
   $(".thumbnail-container").hover(function() {
-    $(this).children(".thumbnail-color").fadeIn(150);
+    $(this).children(".thumbnail-color").fadeIn(300);
     $(this).css("padding-right", "-400px");
-    //Stop Song
-
     //Play song if unmuted
+    //Else animate mute button
   }, function() {
-    $(this).children(".thumbnail-color").fadeOut(150);
+    $(this).children(".thumbnail-color").fadeOut(300);
     $(this).css("padding-right", "0px");
     //Stop Song
   });
+
+  //mute functionality toggle
 });
