45f3cd0d0303c91909b3754baa07a07a7574afc2
Codie Whitaker <CWhitaker2@student.fullsail.edu>
2021-07-24 10:11:38 -0400

    Adding Audio with hover play and reload

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
+var muted = false;
+
+
 $( document ).ready(function() {
   $("#unmuted").hide();
   $("#muted").show();
   $(".thumbnail-color").hide();
 
   $(".thumbnail-container").hover(function() {
+    var track = $(this).children("audio");
     $(this).children(".thumbnail-color").delay(50).show();
     $(this).css("padding-right", "-300px");
-    //Play song if unmuted
-    //Else animate mute button
+    if(muted == false) {
+      //Change Now Playing Content
+      track.get(0).load();
+      track.get(0).play();
+    } else {
+      //Give user feedback that track is muted
+    }
   }, function() {
+    var track = $(this).children("audio");
     $(this).css("padding-right", "0px");
     $(this).children(".thumbnail-color").hide();
-    //Stop Song
+    track.get(0).pause();
   });
 
   //mute functionality toggle
