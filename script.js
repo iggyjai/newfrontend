130de7fc74b611a07805ceb4510fb28fbcd5b2a0
Codie Whitaker <CWhitaker2@student.fullsail.edu>
2021-07-26 14:12:26 -0400

    test

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ $( document ).ready(function() {
 
     if (setAudio) {
       $("#muted-icon") .click(function() {
+        toggleVolume();
         soundEffect.play();
         setAudio = false;
       });
